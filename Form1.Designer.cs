5e058db3f1b1f71999e621cd80ec169e2a383cf2
Camille PERRICHON <camille.perrichon@icof.fr>
2020-12-08 17:02:53 +0100

    a

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,19 +34,21 @@ namespace PPE_Maison_Des_Ligues
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(540, 151);
+            this.button1.Location = new System.Drawing.Point(480, 121);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(173, 136);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "ZIZI";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
