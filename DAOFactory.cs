9c42823a682533eb66da537270a4c1ad6a317e25
Camille PERRICHON <camille.perrichon@icof.fr>
2020-12-08 17:21:56 +0100

    a

--- DAOFactory.cs
+++ DAOFactory.cs
@@ -3,32 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
-
 
 namespace PPE_Maison_Des_Ligues
 {
     class DAOFactory
     {
-        SqlConnection connexion;
-        public DAOFactory()
-        {
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "LAPTOP-KVV6RS2J\\SQLEXPRESS";
-            builder.IntegratedSecurity = true;
-            builder.InitialCatalog = "Banque";
-            Console.WriteLine("Connexion info created : " + builder.ConnectionString);
-            connexion = new SqlConnection(builder.ConnectionString);
-        }
-
-        public void connecter()
-        {
-            connexion.Open();
-        }
-
-        public void deconnecter()
-        {
-            connexion.Close();
-        }
     }
 }
