f4e9e42a50e6198b449c5e85a1dffb4209230bff
Camille PERRICHON <camille.perrichon@icof.fr>
2020-12-15 11:34:47 +0100

    suppression du bouton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,25 +29,13 @@ namespace PPE_Maison_Des_Ligues
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(38, 26);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(195, 170);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ZIZI";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(711, 360);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,8 +44,6 @@ namespace PPE_Maison_Des_Ligues
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
