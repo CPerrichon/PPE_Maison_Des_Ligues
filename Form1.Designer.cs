20398fc246a4c36c75861d6375fa8676fb6debba
Camille PERRICHON <camille.perrichon@icof.fr>
2021-02-23 15:31:16 +0100

    suce

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,14 +29,26 @@ namespace PPE_Maison_Des_Ligues
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 360);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -44,6 +56,8 @@ namespace PPE_Maison_Des_Ligues
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace PPE_Maison_Des_Ligues
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
