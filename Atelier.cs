51ab1222a690852650fc21c3647de504b8e10a46
CPerrichon <62330243+CPerrichon@users.noreply.github.com>
2021-05-06 00:02:10 +0200

    atelier

--- Atelier.cs
+++ Atelier.cs
@@ -16,15 +16,24 @@ namespace PPE_Maison_Des_Ligues
         private DateTime fin;
         private int idParticipant;
         #endregion
+        public Atelier(int numAtelier, string libelleAtelier, int capaciteMax, DateTime debut, DateTime fin, int idParticipant)
+        {
+            this.NumAtelier = numAtelier;
+            this.LibelleAtelier = libelleAtelier;
+            this.CapaciteMax = capaciteMax;
+            this.Debut = debut;
+            this.Fin = fin;
+            this.IdParticipant = idParticipant;
+        }
 
-        #region Constructeur
+        #region Accesseurs
 
-        public Atelier(int unNumAtelier, string unlibelleAtelier, private int unecapaciteMax, DateTime undebut, DateTime unefin, int unidParticipant)
-            {
-                numAtelier = unNumAtelier;
-                libelleAtelier = unLibelleAtelier;
-                
-            }
-
+        public int NumAtelier { get => numAtelier; set => numAtelier = value; }
+        public string LibelleAtelier { get => libelleAtelier; set => libelleAtelier = value; }
+        public int CapaciteMax { get => capaciteMax; set => capaciteMax = value; }
+        public DateTime Debut { get => debut; set => debut = value; }
+        public DateTime Fin { get => fin; set => fin = value; }
+        public int IdParticipant { get => idParticipant; set => idParticipant = value; }
+        #endregion
     }
 }
