69936daa8796313b314d1f8d75498fe9ed599e0e
tanguy-r69 <75259833+tanguy-r69@users.noreply.github.com>
2021-05-17 17:59:04 +0200

    correction classe atelier

--- Atelier.cs
+++ Atelier.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PPE_Maison_Des_Ligues
 {
-    class Atelier
+    public class Atelier
     {
         #region Propriétés privées
         private int numAtelier;
@@ -15,38 +15,64 @@ namespace PPE_Maison_Des_Ligues
         private DateTime debut;
         private DateTime fin;
         private int idParticipant;
-        private List<Atelier> lesAteliers;
+        //private List<Atelier> lesAteliers;
         #endregion
 
         #region Constructeur
 
         public Atelier(int numAtelier, string libelleAtelier, int capaciteMax, DateTime debut, DateTime fin, int idParticipant)
         {
-            this.NumAtelier = numAtelier;
-            this.LibelleAtelier = libelleAtelier;
-            this.CapaciteMax = capaciteMax;
-            this.Debut = debut;
-            this.Fin = fin;
-            this.IdParticipant = idParticipant;
+            this.numAtelier = numAtelier;
+            this.libelleAtelier = libelleAtelier;
+            this.capaciteMax = capaciteMax;
+            this.debut = debut;
+            this.fin = fin;
+            this.idParticipant = idParticipant;
         }
+
         #endregion
 
         #region Accesseurs
-        public int NumAtelier { get => numAtelier; set => numAtelier = value; }
-        public string LibelleAtelier { get => libelleAtelier; set => libelleAtelier = value; }
-        public int CapaciteMax { get => capaciteMax; set => capaciteMax = value; }
-        public DateTime Debut { get => debut; set => debut = value; }
-        public DateTime Fin { get => fin; set => fin = value; }
-        public int IdParticipant { get => idParticipant; set => idParticipant = value; }
-        internal List<Atelier> LesAteliers { get => lesAteliers; set => lesAteliers = value; }
-        #endregion
 
-        #region Méthodes publiques
-
-        public static List<Atelier> listeAteliers() 
+        public int NumAtelier
         {
-            return DAOAtelier.getAllAteliers();
+            get => numAtelier;
+            set => numAtelier = value;
         }
+
+        public string LibelleAtelier
+        {
+            get => libelleAtelier;
+            set => libelleAtelier = value;
+        }
+
+        public int CapaciteMax
+        {
+            get => capaciteMax;
+            set => capaciteMax = value;
+        }
+
+        public DateTime Debut
+        {
+            get => debut;
+            set => debut = value;
+        }
+
+        public DateTime Fin
+        {
+            get => fin;
+            set => fin = value;
+        }
+
+        public int IdParticipant
+        {
+            get => idParticipant;
+            set => idParticipant = value;
+        }
+
+        //internal List<Atelier> LesAteliers { get => lesAteliers; set => lesAteliers = value; }
         #endregion
+
+        
     }
 }
